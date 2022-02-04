44c53ee70f13c1f2254010ece8de11af163695b9
Bert Spaan <bert.spaan@gmail.com>
2022-02-04 15:52:03 +0100

    initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,31 @@
+<script>
+  import { goto } from '$app/navigation'
+  // 9SKv4reG8488MeXs
+  // WSqhn2n3E9cfa6aJ
+
+  let mapId = 'WSqhn2n3E9cfa6aJ'
+
+  const handleSubmit = () => {
+    goto(`/maps/${mapId}`)
+  }
+</script>
+
+<h1>Allmaps Here</h1>
+<form on:submit|preventDefault={handleSubmit}>
+  <input type="text" bind:value={mapId} />
+  <button type="submit"> Show map ID </button>
+</form>
+
+<style>
+  :global(html, body, #svelte) {
+    box-sizing: border-box;
+    margin: 0;
+    height: 100%;
+  }
+  :global(#svelte) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
---
+++ src/routes/maps/[mapId].svelte
@@ -0,0 +1,146 @@
+<script context="module">
+  export async function load({ params, fetch }) {
+    const mapId = params.mapId || '68mcxUe8zJ2Hufws'
+
+    const url = `https://api.allmaps.org/maps/${mapId}`
+    const georef = await fetch(url).then((response) => response.json())
+
+    const imageUri = georef.image.uri
+
+    const imageInfo = await fetch(`${imageUri}/info.json`).then((response) => response.json())
+
+    return {
+      props: {
+        georef,
+        imageInfo
+      }
+    }
+  }
+</script>
+
+<script>
+  import { onMount, onDestroy } from 'svelte'
+
+  import 'ol/ol.css'
+  import Feature from 'ol/Feature.js'
+  import Geolocation from 'ol/Geolocation.js'
+  import Map from 'ol/Map.js'
+  import Point from 'ol/geom/Point.js'
+  import View from 'ol/View.js'
+  import { Circle as CircleStyle, Fill, Stroke, Style } from 'ol/style'
+  import VectorSource from 'ol/source/Vector.js'
+  import IIIF from 'ol/source/IIIF.js'
+  import IIIFInfo from 'ol/format/IIIFInfo.js'
+  import TileLayer from 'ol/layer/Tile.js'
+  import VectorLayer from 'ol/layer/Vector.js'
+
+  import { createTransformer, toImage } from '@allmaps/transform'
+
+  export let georef
+  export let imageInfo
+
+  let olContainer
+  let positionFeature
+
+  function updatePosition(coordinates) {
+    if (positionFeature && coordinates && transformer) {
+      const imageCoordinates = toImage(transformer, coordinates)
+      positionFeature.setGeometry(new Point([imageCoordinates[0], -imageCoordinates[1]]))
+      console.log(coordinates, imageCoordinates)
+    }
+  }
+
+  onMount(async () => {
+    const tileLayer = new TileLayer()
+
+    positionFeature = new Feature()
+
+    positionFeature.setStyle(
+      new Style({
+        image: new CircleStyle({
+          radius: 6,
+          fill: new Fill({
+            color: '#f309CC'
+          }),
+          stroke: new Stroke({
+            color: '#fff',
+            width: 1
+          })
+        })
+      })
+    )
+
+    const vectorLayer = new VectorLayer({
+      source: new VectorSource({
+        features: [positionFeature]
+      })
+    })
+
+    const map = new Map({
+      layers: [tileLayer, vectorLayer],
+      target: olContainer
+    })
+
+    const options = new IIIFInfo(imageInfo).getTileSourceOptions()
+    options.zDirection = -1
+    const iiifTileSource = new IIIF(options)
+    tileLayer.setSource(iiifTileSource)
+    map.setView(
+      new View({
+        resolutions: iiifTileSource.getTileGrid().getResolutions(),
+        extent: iiifTileSource.getTileGrid().getExtent(),
+        constrainOnlyCenter: true
+      })
+    )
+    map.getView().fit(iiifTileSource.getTileGrid().getExtent())
+
+    const geolocation = new Geolocation({
+      tracking: true,
+      trackingOptions: {
+        enableHighAccuracy: true,
+        maximumAge: 100,
+        timeout: 50000
+      }
+    })
+
+    setInterval(() => {
+      updatePosition(geolocation.getPosition())
+    }, 2500)
+
+    geolocation.on('change:position', function () {
+      const coordinates = geolocation.getPosition()
+      updatePosition(coordinates)
+    })
+
+    geolocation.on('error', (error) => {
+      console.error(error)
+    })
+  })
+
+  const transformer = createTransformer(georef.gcps)
+</script>
+
+<svelte:head>
+  <title>Allmaps Here</title>
+</svelte:head>
+
+<div bind:this={olContainer} class="map" />
+
+<style>
+  :global(html, body, #svelte) {
+    box-sizing: border-box;
+    margin: 0;
+    height: 100%;
+  }
+
+  :global(#svelte) {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .map {
+    width: 100%;
+    height: 100%;
+  }
+</style>
